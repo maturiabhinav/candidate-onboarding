b7bfe19862644d8815bf39984d2742013f498bf0
maturiabhinav <abhinav.m@futureinvo.com>
2025-10-15 04:41:31 -0700

    candidate_onboarding

--- candidate_onboarding/routes.py
+++ candidate_onboarding/routes.py
@@ -18,9 +18,9 @@ def get_models():
 def get_s3_client():
     return boto3.client(
         "s3",
-        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-        region_name=os.getenv("AWS_REGION", "us-east-1")
+        access_key_id=os.getenv("ACCESS_KEY_ID"),
+        secret_access_key=os.getenv("SECRET_ACCESS_KEY"),
+        region_name=os.getenv("REGION", "ap-south-1")
     )
 
 def get_s3_bucket():
